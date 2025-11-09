7082dcdb5858ffcacc550d82b2969d9b0e9b7d6e
JensKristianRuben <Jenskristian@gmail.dk>
2025-11-09 21:12:40 +0100

    added node.js server, setted dev script up for running the server, added gitignore

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = Number(process.env.PORT);
+
+app.listen(PORT, () => {
+    console.log("Server is running on port: ", PORT);
+    
+})
