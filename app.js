27d1974ba6f3832ba40e541a7af82839ab26e4b1
JensKristianRuben <Jenskristian@gmail.dk>
2025-11-09 21:27:25 +0100

    added page limiter - max 100 requests for security

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import generalLimiter from './util/generalLimiter.js';
 
 const app = express();
 
+app.use(generalLimiter);
+
+
+
+
 const PORT = Number(process.env.PORT);
 
+
+
+
 app.listen(PORT, () => {
-    console.log("Server is running on port: ", PORT);
-    
-})
+    console.log("Server is running on port: ", PORT); 
+});
