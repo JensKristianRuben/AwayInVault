74f1d8587767092797d89549052fb60c79803070
Jens Kristian Ruben Hansen <jkh@dgibyen.dk>
2025-11-12 10:31:19 +0100

    switched buttons around so they are not inside the form

--- public/sign-in/signIn.js
+++ public/sign-in/signIn.js
@@ -4,8 +4,12 @@ const container = document.getElementById('container');
 
     goRight.addEventListener('click', () => {
       container.classList.add('right-active');
+      goLeft.style.display = 'block';
+      goRight.style.display = 'none';
     });
 
     goLeft.addEventListener('click', () => {
       container.classList.remove('right-active');
+      goLeft.style.display = 'none';
+      goRight.style.display = 'block';
     });
