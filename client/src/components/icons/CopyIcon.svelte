c0f293462ea57dd1d5e0ce9ef046c230c5c29592
JensKristianRuben <Jenskristian79@gmail.dk>
2025-12-16 19:57:57 +0100

    put svg's into components

--- client/src/components/icons/CopyIcon.svelte
+++ client/src/components/icons/CopyIcon.svelte
@@ -1,12 +1,67 @@
 <script>
+  import { scale } from "svelte/transition";
+  import toastr from "toastr";
 
-let isCopied = $state();
+  let isCopied = $state();
 
+  function handleClick() {
+    if (isCopied) return;
 
+    isCopied = true;
+    toastr.success("Password copied")
 
+    setTimeout(() => {
+      isCopied = false;
+    }, 2000);
+  }
 </script>
 
+<button onclick={handleClick} aria-label="Copy to clipboard" class="copy-btn">
+  {#if isCopied}
+    <div in:scale={{ duration: 200, start: 0.5 }} class="icon-wrapper check">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <polyline points="20 6 9 17 4 12"></polyline>
+      </svg>
+    </div>
+  {:else}
+    <div in:scale={{ duration: 200, start: 0.5 }} class="icon-wrapper copy">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <rect width="14" height="14" x="8" y="8" rx="2" ry="2" />
+        <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
+      </svg>
+    </div>
+  {/if}
+</button>
 
-<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/>
-</svg>
+<style>
+
+  .copy {
+    color: white;
+    transition: color 0.2s;
+  }
+
+
+  .check {
+    color: #00ff80;
+  }
+</style>
